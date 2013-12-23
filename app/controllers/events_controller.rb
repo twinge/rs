64670bf72a376aacbc94efcd1eb15c60e3eba4d1
Spencer Oberstadt <soberstadt@gmail.com>
2013-12-23 15:55:55 -0600

    more code cleanup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,33 +22,24 @@ class EventsController < ApplicationController
         @spaces += driver.number_passengers
 				@riders_done+=driver.current_passengers_number
       end
+
+      # find the avg lat/long
       @latitude_avg=0
       @longitude_avg=0
       count=0
       @locations=Hash.new;
-      @drivers.each do |driver|
-        location=driver.latitude.to_s+" "+driver.longitude.to_s
+      current_event.rides.each do |ride|
+        location=ride.latitude.to_s+" "+ride.longitude.to_s
         if !@locations.has_key?(location)
-          @locations[location]={:latitude => driver.latitude, :longitude => driver.longitude, :rides => Array.new}
+          @locations[location]={:latitude => ride.latitude, :longitude => ride.longitude, :rides => Array.new}
         end
-        if (driver.latitude && driver.latitude != 0 && driver.longitude && driver.longitude != 0)
-          @latitude_avg+=driver.latitude
-          @longitude_avg+=driver.longitude
+        if (ride.latitude && ride.latitude != 0 && ride.longitude && ride.longitude != 0)
+          @latitude_avg+=ride.latitude
+          @longitude_avg+=ride.longitude
           count+=1
         end
-        @locations[location][:rides].push({:type => "driver",:id => driver.id})
-      end
-      @riders.each do |rider|
-        location=rider.latitude.to_s+" "+rider.longitude.to_s
-        if !@locations.has_key?(location)
-          @locations[location]={:latitude => rider.latitude, :longitude => rider.longitude, :rides => Array.new}
-        end
-        if (rider.latitude != 0 && rider.longitude != 0)
-          @latitude_avg+=rider.latitude
-          @longitude_avg+=rider.longitude
-          count+=1
-        end
-        @locations[location][:rides].push({:type => "rider",:id => rider.id})
+        type = ride.drive_willingness == 0 ? "rider" : "driver"
+        @locations[location][:rides].push({:type => type,:id => ride.id})
       end
       if count != 0
         @latitude_avg/=count
@@ -57,9 +48,9 @@ class EventsController < ApplicationController
         @latitude_avg=0
         @longitude_avg=0
       end
-      @message =false
+
       @help_rides = Ride.where(:latitude => 0, :longitude => 0, :event_id => current_event.id).includes(:person)
-      @message = true if @help_rides.size > 0
+      @message = (@help_rides.size > 0)
     end
   end
 
